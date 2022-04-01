a16d32578b23ef6461526f8b1621f3ae544bb5eb
rsobreira93 <sobreiraromuloes@gmail.com>
2022-04-01 07:30:53 -0300

    refactor: create category use case

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,19 +1,13 @@
 import { Router } from "express";
 
 import { CategoriesRepository } from "../modules/cars/repositories/CategoriesRepository";
-import { CreateCategoryService } from "../modules/cars/services/CreateCategoryService";
+import { createcategoryController } from "../modules/cars/useCases/createCategory";
 
 const categoriesRoutes = Router();
 const categoriesRepository = new CategoriesRepository();
 
 categoriesRoutes.post("/", (request, response) => {
-  const { name, description } = request.body;
-
-  const createCategoryService = new CreateCategoryService(categoriesRepository);
-
-  const category = createCategoryService.execute({ name, description });
-
-  return response.status(201).json(category);
+  return createcategoryController.handle(request, response);
 });
 
 categoriesRoutes.get("/", (request, response) => {
