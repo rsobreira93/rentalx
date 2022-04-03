ebdf4647b3a9b7197e1c025a260b0fc37dec3464
rsobreira93 <sobreiraromuloes@gmail.com>
2022-04-03 09:23:17 -0300

    feat: add migrations

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,13 +1,15 @@
-import { createConnection, getConnectionOptions } from "typeorm";
+import { createConnection } from "typeorm";
 
-interface IOptions {
-  host: string;
-}
+createConnection();
 
-getConnectionOptions().then((options) => {
-  const newOptions = options as IOptions;
-  newOptions.host = "database";
-  createConnection({
-    ...options,
-  });
-});
+// interface IOptions {
+//   host: string;
+// }
+
+// getConnectionOptions().then((options) => {
+//   const newOptions = options as IOptions;
+//   newOptions.host = "database";
+//   createConnection({
+//     ...options,
+//   });
+// });
