e71a1da4e239d00d15be71f9e72dd5cea635f8ed
rsobreira93 <sobreiraromuloes@gmail.com>
2022-04-04 08:28:27 -0300

    feat: ensure CreateUserUseCase

---
+++ src/modules/accounts/useCases/CreateUserUseCase.ts
@@ -0,0 +1,33 @@
+import { inject, injectable } from "tsyringe";
+
+import { ICreateUserDTO } from "../dtos/ICreateUserDTO";
+import { User } from "../entities/User";
+import { IUsersRepository } from "../repositories/IUsersRepository";
+
+@injectable()
+class CreateUserUseCase {
+  constructor(
+    @inject("UsersRepository")
+    private usersRepository: IUsersRepository
+  ) {}
+
+  async execute({
+    email,
+    name,
+    driver_license,
+    password,
+    username,
+  }: ICreateUserDTO): Promise<User> {
+    const user = await this.usersRepository.create({
+      email,
+      name,
+      driver_license,
+      password,
+      username,
+    });
+
+    return user;
+  }
+}
+
+export { CreateUserUseCase };
