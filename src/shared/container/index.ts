a8b046f6f9cdb8306c7f1ead780b0b19c5449733
rsobreira93 <sobreiraromuloes@gmail.com>
2022-04-07 07:40:48 -0300

    feat: inject car

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -2,6 +2,7 @@ import { UsersRepository } from "@modules/accounts/infra/typeorm/repositories/Us
 import { IUsersRepository } from "@modules/accounts/repositories/IUsersRepository";
 import { CategoriesRepository } from "@modules/cars/infra/typeorm/repositories/CategoriesRepository";
 import { SpecificationRepository } from "@modules/cars/infra/typeorm/repositories/SpecificationsRepository";
+import { ICarRepository } from "@modules/cars/repositories/ICarsRepository";
 import { ICategoriesRepository } from "@modules/cars/repositories/ICategoriesRepository";
 import { ISpecifiactionsRepository } from "@modules/cars/repositories/ISpecificationsRepository";
 import { container } from "tsyringe";
@@ -20,3 +21,5 @@ container.registerSingleton<IUsersRepository>(
   "UsersRepository",
   UsersRepository
 );
+
+container.registerSingleton<ICarRepository>("CarsRepository", CarsRepository);
