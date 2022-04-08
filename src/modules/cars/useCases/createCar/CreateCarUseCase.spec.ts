0a8b14f741c2e43b3c145f4e535369c2446765c3
rsobreira93 <sobreiraromuloes@gmail.com>
2022-04-08 07:38:46 -0300

    tests: ensure CreateCarUseCase

--- src/modules/cars/useCases/createCar/CreateCarUseCase.spec.ts
+++ src/modules/cars/useCases/createCar/CreateCarUseCase.spec.ts
@@ -1,5 +1,7 @@
 import { CarRepositoryInMemory } from "@modules/cars/repositories/in-memory/CarsRepositoryInMemory";
 
+import { AppError } from "@shared/errors/AppError";
+
 import { CreateCarUseCase } from "./CreateCarUseCase";
 
 let createCarUseCase: CreateCarUseCase;
@@ -21,5 +23,44 @@ describe("Create Car", () => {
       license_plate: "ABC-1234",
       name: "Carro de teste",
     });
+  });
+
+  it("Should not be able to create a new car with the same license plate", async () => {
+    await createCarUseCase.execute({
+      brand: "Brand",
+      category_id: "catetgory",
+      daily_rate: 100,
+      description: "Carro de teste",
+      fine_amount: 60,
+      license_plate: "ABC-1234",
+      name: "Carro de teste",
+    });
+
+    await expect(
+      createCarUseCase.execute({
+        brand: "Brand",
+        category_id: "catetgory",
+        daily_rate: 100,
+        description: "Carro de teste",
+        fine_amount: 60,
+        license_plate: "ABC-1234",
+        name: "Carro de teste",
+      })
+    ).rejects.toBeInstanceOf(AppError);
+  });
+
+  it("Should be able to create a new car with avaliable true by default", async () => {
+    const car = await createCarUseCase.execute({
+      brand: "Brand",
+      category_id: "catetgory",
+      daily_rate: 100,
+      description: "Carro de teste",
+      fine_amount: 60,
+      license_plate: "ABCD-1234",
+      name: "Car Available",
+    });
+
+    expect(car).toHaveProperty("id");
+    expect(car.avaliable).toBe(true);
   });
 });
