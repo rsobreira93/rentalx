1b38a3dafa27053c58583047a7c235b378b41c0e
rsobreira93 <sobreiraromuloes@gmail.com>
2022-04-09 10:10:07 -0300

    feat: router for list avaliable cars

--- src/shared/infra/http/routes/cars.routes.ts
+++ src/shared/infra/http/routes/cars.routes.ts
@@ -1,4 +1,5 @@
 import { CreateCarController } from "@modules/cars/useCases/createCar/CreateCarController";
+import { ListAvailableCarsController } from "@modules/cars/useCases/listAvailableCars/ListAvailableCarsController";
 import { Router } from "express";
 
 import { ensureAdmin } from "@shared/infra/http/middlewares/ensureAdmin";
@@ -6,7 +7,9 @@ import { ensureAuthenticated } from "@shared/infra/http/middlewares/ensureAthent
 
 const carsRoutes = Router();
 const carsController = new CreateCarController();
+const listAvailableCarsController = new ListAvailableCarsController();
 
 carsRoutes.post("/", ensureAuthenticated, ensureAdmin, carsController.handle);
 
+carsRoutes.get("/available", listAvailableCarsController.handle);
 export { carsRoutes };
