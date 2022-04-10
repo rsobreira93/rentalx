e4fbebf36c7237422263befff07da0604c7fdf1f
rsobreira93 <sobreiraromuloes@gmail.com>
2022-04-10 08:40:28 -0300

    feat: add route specfication

--- src/shared/infra/http/routes/cars.routes.ts
+++ src/shared/infra/http/routes/cars.routes.ts
@@ -1,4 +1,5 @@
 import { CreateCarController } from "@modules/cars/useCases/createCar/CreateCarController";
+import { CreateCarSpecificationController } from "@modules/cars/useCases/createCarSpecification/CreateCarSpecificationController";
 import { ListAvailableCarsController } from "@modules/cars/useCases/listAvailableCars/ListAvailableCarsController";
 import { Router } from "express";
 
@@ -8,8 +9,16 @@ import { ensureAuthenticated } from "@shared/infra/http/middlewares/ensureAthent
 const carsRoutes = Router();
 const carsController = new CreateCarController();
 const listAvailableCarsController = new ListAvailableCarsController();
+const carSpecificationController = new CreateCarSpecificationController();
 
 carsRoutes.post("/", ensureAuthenticated, ensureAdmin, carsController.handle);
 
 carsRoutes.get("/available", listAvailableCarsController.handle);
+
+carsRoutes.post(
+  "/specifications/:id",
+  ensureAuthenticated,
+  ensureAdmin,
+  carSpecificationController.handle
+);
 export { carsRoutes };
