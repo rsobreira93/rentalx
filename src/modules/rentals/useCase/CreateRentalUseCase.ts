30d3bc949d201da33bee293a728f353191ea712c
rsobreira93 <sobreiraromuloes@gmail.com>
2022-04-12 08:38:52 -0300

    feat: add CreateRentalController

---
+++ src/modules/rentals/useCase/CreateRentalUseCase.ts
@@ -0,0 +1,68 @@
+import { inject, injectable } from "tsyringe";
+
+import { IDateProvider } from "@shared/container/providers/DateProvider/IDateProvider";
+import { AppError } from "@shared/errors/AppError";
+
+import { Rental } from "../infra/typeorm/entities/Rental";
+import { IRentalsRepository } from "../repositories/IRentalsRepository";
+
+interface IRequest {
+  user_id: string;
+  car_id: string;
+  expected_return_date: Date;
+}
+
+@injectable()
+class CreateRentalUseCase {
+  constructor(
+    @inject("DayjsDataProvider")
+    private dateProvider: IDateProvider,
+
+    @inject("RentalsRepository")
+    private rentalsRepository: IRentalsRepository
+  ) {}
+
+  async execute({
+    car_id,
+    user_id,
+    expected_return_date,
+  }: IRequest): Promise<Rental> {
+    const minimumHours = 24;
+
+    const carUnavailable = await this.rentalsRepository.findOpenRentalByCar(
+      car_id
+    );
+
+    if (carUnavailable) {
+      throw new AppError("Car is already rented");
+    }
+
+    const dateNow = this.dateProvider.dateNow();
+    const compare = this.dateProvider.compareInHours(
+      dateNow,
+      expected_return_date
+    );
+
+    if (compare < minimumHours) {
+      throw new AppError("Expected return date must be at least 24 hours");
+    }
+
+    const rentalOpenToUser = await this.rentalsRepository.findOpenRentalByUser(
+      user_id
+    );
+
+    if (rentalOpenToUser) {
+      throw new AppError("User already has an open rental");
+    }
+
+    const rental = await this.rentalsRepository.create({
+      car_id,
+      user_id,
+      expected_return_date,
+    });
+
+    return rental;
+  }
+}
+
+export { CreateRentalUseCase };
