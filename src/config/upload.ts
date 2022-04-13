68fc9970efd082f02cb5748fc5b31b116b329ca5
rsobreira93 <sobreiraromuloes@gmail.com>
2022-04-13 08:42:24 -0300

    rerfactor: adjust upload dos not const are object

--- src/config/upload.ts
+++ src/config/upload.ts
@@ -4,29 +4,16 @@ import { resolve } from "path";
 
 export default {
   upload(folder: string) {
-    return multer({
+    return {
       storage: multer.diskStorage({
         destination: resolve(__dirname, "..", "..", folder),
         filename: (request, file, callback) => {
           const fileHash = crypto.randomBytes(16).toString("hex");
           const fileName = `${fileHash}-${file.originalname}`;
+
           return callback(null, fileName);
         },
       }),
-      fileFilter: (request, file, callback) => {
-        const allowedMimes = [
-          "image/jpeg",
-          "image/pjpeg",
-          "image/png",
-          "image/gif",
-        ];
-
-        if (allowedMimes.includes(file.mimetype)) {
-          return callback(null, true);
-        }
-
-        return callback(new Error("Invalid file type."));
-      },
-    });
+    };
   },
 };
