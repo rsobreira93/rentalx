008c3a46204565c557c3a607a27deadb39553806
rsobreira93 <sobreiraromuloes@gmail.com>
2022-04-17 11:51:17 -0300

    feat: add repository from userToken

---
+++ src/modules/accounts/infra/typeorm/repositories/UsersTokensRepository.ts
@@ -0,0 +1,31 @@
+import { ICreateUserTokensDTO } from "@modules/accounts/dtos/ICreateUserTokensDTO";
+import { IUsersTokensRepository } from "@modules/accounts/repositories/IUsersTokensRepository";
+import { getRepository, Repository } from "typeorm";
+
+import { UserTokens } from "../entities/UserTokens";
+
+class UsersTokensRepository implements IUsersTokensRepository {
+  private repository: Repository<UserTokens>;
+
+  constructor() {
+    this.repository = getRepository(UserTokens);
+  }
+
+  async create({
+    expires_date,
+    refresh_token,
+    user_id,
+  }: ICreateUserTokensDTO): Promise<UserTokens> {
+    const userToken = this.repository.create({
+      user_id,
+      expires_date,
+      refresh_token,
+    });
+
+    await this.repository.save(userToken);
+
+    return userToken;
+  }
+}
+
+export { UsersTokensRepository };
