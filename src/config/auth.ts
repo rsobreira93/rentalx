a15591ccf3e7bdff0542ee3b5d9ae69ac74fd78f
rsobreira93 <sobreiraromuloes@gmail.com>
2022-04-18 07:19:48 -0300

    feat: add secrets to auth

---
+++ src/config/auth.ts
@@ -0,0 +1,7 @@
+export default {
+  secret_token: "5afd22e6c29ba0cf5510e8dc2d3c0a07",
+  expires_in_token: "15m",
+  secret_refresh_token: "482f0aac8a7aed78eb83f163451ef1e8",
+  expires_in_refresh_token: "30d",
+  refresh_token_expires_days: 30,
+};
