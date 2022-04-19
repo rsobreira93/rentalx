bbc59ba50068f7b2bc8dd2829939b8e4d810557b
rsobreira93 <sobreiraromuloes@gmail.com>
2022-04-19 07:31:26 -0300

    feat: add new route

--- src/shared/infra/http/routes/authenticate.routes.ts
+++ src/shared/infra/http/routes/authenticate.routes.ts
@@ -1,9 +1,12 @@
 import { AuthenticateUserController } from "@modules/accounts/useCases/AuthenticateUser/AuthenticateUserController";
+import { RefreshTokenController } from "@modules/accounts/useCases/refreshToken/RefreshTokenController";
 import { Router } from "express";
 
 const authenticateRoutes = Router();
 const athenticateControler = new AuthenticateUserController();
+const refreshTokenController = new RefreshTokenController();
 
 authenticateRoutes.post("/sessions", athenticateControler.handle);
+authenticateRoutes.post("/refresh_token", refreshTokenController.handle);
 
 export { authenticateRoutes };
