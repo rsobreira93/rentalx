7f1e8868a3e032d4052f61eefcc347493037f6fa
rsobreira93 <sobreiraromuloes@gmail.com>
2022-04-20 07:29:41 -0300

    feat: add EtherealMailProvider

--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -3,8 +3,15 @@ import { container } from "tsyringe";
 import { IDateProvider } from "@shared/container/providers/DateProvider/IDateProvider";
 
 import { DayjsDateProvider } from "./DateProvider/implementations/DayjsDateProvider";
+import { IMailProvider } from "./MailProvider/IMailProvider";
+import { EtherealMailProvider } from "./MailProvider/implementations/EtherealMailProvider";
 
 container.registerSingleton<IDateProvider>(
   "DayjsDataProvider",
   DayjsDateProvider
+);
+
+container.registerInstance<IMailProvider>(
+  "EtherealMailProvider",
+  new EtherealMailProvider()
 );
