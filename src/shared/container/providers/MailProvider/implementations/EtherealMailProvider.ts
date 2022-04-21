65f22661fe4522be353b94f0fa467343bd41a081
rsobreira93 <sobreiraromuloes@gmail.com>
2022-04-21 08:25:29 -0300

    rerfactor: alter paramter to implementation handlebars

--- src/shared/container/providers/MailProvider/implementations/EtherealMailProvider.ts
+++ src/shared/container/providers/MailProvider/implementations/EtherealMailProvider.ts
@@ -1,3 +1,5 @@
+import fs from "fs";
+import handlebars from "handlebars";
 import nodemailer, { Transporter } from "nodemailer";
 import { injectable } from "tsyringe";
 
@@ -26,13 +28,23 @@ class EtherealMailProvider implements IMailProvider {
       .catch((err) => console.error);
   }
 
-  async sendMail(to: string, subject: string, body: string): Promise<void> {
+  async sendMail(
+    to: string,
+    subject: string,
+    variables: any,
+    path: string
+  ): Promise<void> {
+    const templateFileContent = fs.readFileSync(path).toString("utf-8");
+
+    const templateParse = handlebars.compile(templateFileContent);
+
+    const templateHtml = templateParse(variables);
+
     const message = await this.client.sendMail({
       to,
       from: "Rentx <noreplay@rentex.com.br>",
       subject,
-      text: body,
-      html: body,
+      html: templateHtml,
     });
 
     console.log("Message sent: %s", message.messageId);
