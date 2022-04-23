d73f5884ccb0971434ba2b08f94097e17349076a
rsobreira93 <sobreiraromuloes@gmail.com>
2022-04-23 11:06:47 -0300

    feat: add upload image to bucket or local

--- src/modules/cars/useCases/uploadCarImage/UploadCarImageUseCase.ts
+++ src/modules/cars/useCases/uploadCarImage/UploadCarImageUseCase.ts
@@ -1,6 +1,8 @@
 import { CarsImagesRepository } from "@modules/cars/infra/typeorm/repositories/CarsImagesRepository";
 import { inject, injectable } from "tsyringe";
 
+import { IStorageProvider } from "@shared/container/providers/StorageProvider/IStorageProvider";
+
 interface IRquest {
   car_id: string;
   images_name: string[];
@@ -10,11 +12,15 @@ interface IRquest {
 class UploadCarImageUseCase {
   constructor(
     @inject("CarsImagesRepository")
-    private carsImagesRepository: CarsImagesRepository
+    private carsImagesRepository: CarsImagesRepository,
+
+    @inject("StorageProvider")
+    private storageProvider: IStorageProvider
   ) {}
   async execute({ car_id, images_name }: IRquest) {
     images_name.map(async (image) => {
       await this.carsImagesRepository.create(car_id, image);
+      await this.storageProvider.saveFile(image, "cars");
     });
   }
 }
