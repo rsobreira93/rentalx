3ec78b8df69059904a94ced2019c8ff6365bd496
rsobreira93 <sobreiraromuloes@gmail.com>
2022-04-23 09:39:56 -0300

    refactor: adjust UploadAvatarUseCase

--- src/modules/accounts/useCases/updateUserAvatar/UpdateAvatarUseCase.ts
+++ src/modules/accounts/useCases/updateUserAvatar/UpdateAvatarUseCase.ts
@@ -1,6 +1,7 @@
 import { IUsersRepository } from "@modules/accounts/repositories/IUsersRepository";
-import { deleFile } from "@utils/file";
 import { inject, injectable } from "tsyringe";
+
+import { IStorageProvider } from "@shared/container/providers/StorageProvider/IStorageProvider";
 
 interface IRequest {
   user_id: string;
@@ -11,15 +12,21 @@ interface IRequest {
 class UpdateAvatarUseCase {
   constructor(
     @inject("UsersRepository")
-    private usersRepository: IUsersRepository
+    private usersRepository: IUsersRepository,
+
+    @inject("StorageProvider")
+    private storageProvider: IStorageProvider
   ) {}
   async execute({ avatar_file, user_id }: IRequest): Promise<void> {
     const user = await this.usersRepository.findById(user_id);
 
+    console.log(user.avatar);
     if (user.avatar) {
-      await deleFile(`./tmp/avatar/${user.avatar}`);
+      await this.storageProvider.deleteFile(user.avatar, "avatar");
     }
+
     user.avatar = avatar_file;
+    await this.storageProvider.saveFile(user.avatar, "avatar");
 
     await this.usersRepository.create(user);
   }
