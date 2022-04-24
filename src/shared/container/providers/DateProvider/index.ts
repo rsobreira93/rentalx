2e8a1c518987631b7d008698625c9826fa8c4354
rsobreira93 <sobreiraromuloes@gmail.com>
2022-04-24 11:55:27 -0300

    refactor: split index providers

---
+++ src/shared/container/providers/DateProvider/index.ts
@@ -0,0 +1,10 @@
+import { container } from "tsyringe";
+
+import { IDateProvider } from "@shared/container/providers/DateProvider/IDateProvider";
+
+import { DayjsDateProvider } from "./implementations/DayjsDateProvider";
+
+container.registerSingleton<IDateProvider>(
+  "DayjsDataProvider",
+  DayjsDateProvider
+);
