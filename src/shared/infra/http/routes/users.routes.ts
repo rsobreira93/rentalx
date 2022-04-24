8e2133e1e9787756252a519abf7e2d685272b32c
rsobreira93 <sobreiraromuloes@gmail.com>
2022-04-24 08:30:34 -0300

    feat: add new route profile

--- src/shared/infra/http/routes/users.routes.ts
+++ src/shared/infra/http/routes/users.routes.ts
@@ -1,5 +1,6 @@
 import uploadConfig from "@config/upload";
 import { CreateUserController } from "@modules/accounts/useCases/createUser/CreateUserController";
+import { ProfileUserController } from "@modules/accounts/useCases/profileUserUseCase/ProfileUserController";
 import { UpdateAvatarController } from "@modules/accounts/useCases/updateUserAvatar/UpdateAvatarController";
 import { Router } from "express";
 import multer from "multer";
@@ -12,6 +13,7 @@ const uploadAvatar = multer(uploadConfig);
 
 const userController = new CreateUserController();
 const updateAvatarController = new UpdateAvatarController();
+const profileUserController = new ProfileUserController();
 
 usersRouters.post("/", userController.handle);
 
@@ -21,5 +23,7 @@ usersRouters.patch(
   uploadAvatar.single("avatar"),
   updateAvatarController.handle
 );
+
+usersRouters.get("/profile", ensureAuthenticated, profileUserController.handle);
 
 export { usersRouters };
